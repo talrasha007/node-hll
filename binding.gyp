798d6f7ec35b087a4c5615da91befd1a4e1ef6b3
ttl <talrasha007@gmail.com>
2017-07-20 18:15:47 +0800

    HLL wrap.

--- binding.gyp
+++ binding.gyp
@@ -4,7 +4,8 @@
             "target_name": "node-hll",
             "sources": [
                 "src/module.cc",
-                "deps/hll/src/hll.c"
+                "deps/hll/src/hll.c",
+                "deps/hll/deps/MurmurHash3/MurmurHash3.c"
             ],
             "include_dirs": [
                 "<!(node -e \"require('nan')\")",
---
+++ src/module.cc
@@ -0,0 +1,75 @@
+#include <math.h>
+#include <nnu.h>
+
+extern "C" {
+    #include <hll.h>
+}
+
+class HLLWrap: public nnu::ClassWrap<HLLWrap> {
+public:
+    static const char * const CLASS_NAME;
+
+    static void setupMember(v8::Local<v8::FunctionTemplate>& tpl) {
+        Nan::SetPrototypeMethod(tpl, "add", wrapFunction<&HLLWrap::add>);
+        Nan::SetPrototypeMethod(tpl, "count", wrapFunction<&HLLWrap::count>);
+        Nan::SetPrototypeMethod(tpl, "merge", wrapFunction<&HLLWrap::merge>);
+    }
+
+    static NAN_METHOD(ctor) {
+        HLLWrap *ptr = NULL;
+        v8::Local<v8::Value> arg = info[0];
+
+        if (arg->IsNumber()) {
+            ptr = new HLLWrap(arg->Uint32Value());
+        } else {
+            const char *registers = node::Buffer::Data(info[0]);
+            size_t size = node::Buffer::Length(info[0]);
+            ptr = new HLLWrap(registers, size);
+        }
+
+        ptr->Wrap(info.This());
+        info.GetReturnValue().Set(info.This());
+    }
+
+private:
+    HLLWrap(uint8_t bits) {
+        hll_init(&hll, bits);
+    }
+
+    HLLWrap(const char *buffer, size_t size) {
+        hll_load(&hll, buffer, size);
+    }
+
+    ~HLLWrap() {
+        hll_destroy(&hll);
+    }
+
+private:
+    NAN_METHOD(add) {
+        const char *data = node::Buffer::Data(info[0]);
+        size_t len = node::Buffer::Length(info[0]);
+
+        hll_add(&hll, data, len);
+    }
+
+    NAN_METHOD(count) {
+        double cnt = hll_count(&this->hll);
+        info.GetReturnValue().Set(Nan::New(round(cnt)));
+    }
+
+    NAN_METHOD(merge) {
+        HLLWrap * src = Nan::ObjectWrap::Unwrap<HLLWrap>(info[0]->ToObject());
+        hll_merge(&hll, &src->hll);
+    }
+
+private:
+    HLL hll;
+};
+
+const char * const HLLWrap::CLASS_NAME = "HLL";
+
+NAN_MODULE_INIT(initAll) {
+    HLLWrap::setup(target);
+}
+
+NODE_MODULE(hll, initAll);
